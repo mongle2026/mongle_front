35e887a24461e2eb86805d1cce6556708400a7b1
Yoojin-J <syoojin03@gmail.com>
2026-05-26 01:13:17 +0900

    feat: app.js Stack.Navigator 추가

---
+++ features/LetterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const LetterPage = () => {
+  return (
+    <View>
+      <Text>LetterPage</Text>
+    </View>
+  )
+}
+
+export default LetterPage
