96b4fba9731306a3701a81525e0ea84cfcd6fc90
jenklm <126846455+jenklm@users.noreply.github.com>
2026-05-28 19:30:41 +0900

    feat: SelectRecipient and SelectMusic 화면 연결

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { View, StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
-import TopNavigation from './shared/components/TopNavigation';
-import SelectRecipient from './src/write/SelectRecipient.jsx';
-import SelectMusic from './src/write/SelectMusic.jsx';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SelectRecipient from './src/write/SelectRecipient';
+import SelectMusic from './src/write/SelectMusic';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,17 +21,14 @@ export default function App() {
 
   return (
     <SafeAreaProvider>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <SelectMusic />
-      </View>
+      <StatusBar style="auto" />
+
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="SelectRecipient" component={SelectRecipient} />
+          <Stack.Screen name="SelectMusic" component={SelectMusic} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
