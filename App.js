d5eca6cf7f0c5f73a5702d8cf7c43963d06a28e8
jenklm <126846455+jenklm@users.noreply.github.com>
2026-06-02 12:55:10 +0900

    feat: Img components

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useFonts } from 'expo-font';
-import LetterScreen from './features/write/letter/LetterScreen';
 
+import LetterScreen from './features/write/letter/LetterScreen';
+import Img from './features/write/components/Img';
+
+const Stack = createNativeStackNavigator();
+
+function ImgTestScreen() {
+  const [selectedImage, setSelectedImage] = useState(
+    require('./assets/write/cover_img.png')
+  );
+
+  return (
+    <View style={styles.container}>
+      <Img
+        imageSource={selectedImage}
+        onPressDelete={() => setSelectedImage(null)}
+        style={styles.imageItem}
+      />
+
+      <Img
+        onPressAdd={() => console.log('이미지 추가')}
+        style={styles.imageItem}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,23 +37,32 @@ export default function App() {
     'Pretendard-Medium': require('./assets/fonts/Pretendard-Medium.ttf'),
     'Pretendard-SemiBold': require('./assets/fonts/Pretendard-SemiBold.ttf'),
   });
-  const Stack = createNativeStackNavigator();
-
 
   if (!fontsLoaded) return null;
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen
-          name="Letter"
-          component={LetterScreen}
-        />
+      <StatusBar style="auto" />
+
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="ImgTest" component={ImgTestScreen} />
+        <Stack.Screen name="Letter" component={LetterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: 20,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  imageItem: {
+    flex: 1,
+  },
+});
