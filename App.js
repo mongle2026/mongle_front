66c33c4e2874b372ae1fd70840655163d7532035
Hodu <9aaa18@gmail.com>
2026-06-03 00:50:36 +0900

    remove: App.js의 TabBar 삭제

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; // 추가
-import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
-import { StyleSheet, Text, View } from 'react-native';
-import TabBar from './shared/components/TabBar'; // 추가
 import LetterScreen from './features/write/letter/LetterScreen';
 import LetterCoverSelect from './features/write/letter/LetterCoverSelect';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator(); // 추가
-
-function PlaceholderScreen() { // 추가: 탭 화면 확정 전 임시 화면
-  return <View style={{ flex: 1 }} />;
-}
-
-function WriteStack() { // 추가: 쓰기 탭 내부 스택
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="LetterCoverSelect" component={LetterCoverSelect} />
-      <Stack.Screen name="Letter" component={LetterScreen} />
-    </Stack.Navigator>
-  );
-}
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -35,16 +17,10 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      {/* 추가: 커스텀 TabBar 연결 */}
-      <Tab.Navigator
-        tabBar={(props) => <TabBar {...props} />}
-        screenOptions={{ headerShown: false }}
-      >
-        <Tab.Screen name="탭1" options={{ tabBarLabel: '탭1' }} component={PlaceholderScreen} />
-        <Tab.Screen name="탭2" options={{ tabBarLabel: '탭2' }} component={WriteStack} />
-        <Tab.Screen name="탭3" options={{ tabBarLabel: '탭3' }} component={PlaceholderScreen} />
-        <Tab.Screen name="탭4" options={{ tabBarLabel: '탭4' }} component={PlaceholderScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="LetterCoverSelect" component={LetterCoverSelect} />
+        <Stack.Screen name="Letter" component={LetterScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
