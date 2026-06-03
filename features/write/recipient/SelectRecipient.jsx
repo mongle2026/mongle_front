17c7338b8e08177c06c261cec731a135c2ff3fb3
Yoojin-J <syoojin03@gmail.com>
2026-06-04 02:18:40 +0900

    feat: Empty 추가

--- features/write/recipient/SelectRecipient.jsx
+++ features/write/recipient/SelectRecipient.jsx
@@ -3,6 +3,7 @@ import { View, FlatList } from 'react-native';
 import TopNavigation from '../../../shared/components/TopNavigation';
 import SearchField from '../../../shared/components/SearchField';
 import ListRow from '../components/ListRow';
+import Empty from '../../../shared/components/Empty';
 
 import UseSelectRecipient from './hook/UseSelectRecipient';
 
@@ -36,21 +37,28 @@ export default function SelectRecipient({ navigation }) {
         placeholder="편지를 받을 사람을 검색해 주세요."
       />
 
-      <FlatList
-        data={userList}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <ListRow
-            title={item.nickname}
-            subtitle={item.username}
-            img={item.image}
-            imageSource={item.imageSource}
-            caption
-            selected={selectedRecipientId === item.id}
-            onPress={() => handleSelectRecipient(item.id)}
-          />
-        )}
-      />
+      {keyword && userList.length === 0 ? (
+        <Empty
+          title='일치하는 사용자가 없습니다.'
+          body='닉네임이나 아이디를 다시 확인해 보세요.'
+        />
+      ) : (
+        <FlatList
+          data={userList}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <ListRow
+              title={item.nickname}
+              subtitle={item.username}
+              img={item.image}
+              imageSource={item.imageSource}
+              caption
+              selected={selectedRecipientId === item.id}
+              onPress={() => handleSelectRecipient(item.id)}
+            />
+          )}
+        />
+      )}
     </View>
   );
 }
