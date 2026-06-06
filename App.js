6730ea38f1249414ac5fff76f6450c7c38d1bf37
jenklm <126846455+jenklm@users.noreply.github.com>
2026-06-06 17:05:39 +0900

    fix: recipient profile image changement

--- App.js
+++ App.js
@@ -2,45 +2,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
-import { View, StyleSheet } from 'react-native';
 
-import ListRow from './features/write/components/ListRow';
-import { colors } from './shared/styles/color';
+import SelectRecipientScreen from './features/write/recipient/SelectRecipientScreen.jsx';
+import SelectMusic from './features/write/music/SelectMusic.jsx';
 
 const Stack = createNativeStackNavigator();
-
-function ListRowTestScreen() {
-  return (
-    <View style={styles.container}>
-      <ListRow
-        title="name(나)"
-        subtitle="@memeow"
-        img="profile"
-        caption
-        selected={false}
-        onPress={() => console.log('첫 번째 선택')}
-      />
-
-      <ListRow
-        title="코코"
-        subtitle="@cocokim"
-        img="profile"
-        caption
-        selected
-        onPress={() => console.log('두 번째 선택')}
-      />
-
-      <ListRow
-        title="Warm On A Cold Night"
-        subtitle="Honne"
-        img="music"
-        caption
-        selected={false}
-        onPress={() => console.log('음악 선택')}
-      />
-    </View>
-  );
-}
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -57,18 +23,14 @@ export default function App() {
 
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen
-          name="ListRowTest"
-          component={ListRowTestScreen}
+          name="SelectRecipientScreen"
+          component={SelectRecipientScreen}
+        />
+        <Stack.Screen
+          name="SelectMusic"
+          component={SelectMusic}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: colors.bgDefault,
-  },
-});
--- features/write/recipient/SelectRecipientScreen.jsx
+++ features/write/recipient/SelectRecipientScreen.jsx
@@ -3,7 +3,6 @@ import { View, FlatList } from 'react-native';
 import TopNavigation from '../../../shared/components/TopNavigation';
 import SearchField from '../../../shared/components/SearchField';
 import ListRow from '../components/ListRow';
-import Empty from '../../../shared/components/Empty';
 
 import UseSelectRecipient from './hook/UseSelectRecipient';
 
@@ -11,14 +10,12 @@ export default function SelectRecipient({ navigation }) {
   const {
     keyword,
     filteredRecipients,
-    userList,
     selectedRecipientId,
     isNextEnabled,
     handleChangeKeyword,
     handleFocusSearch,
     handleSelectRecipient,
-    handlePressNext,
-  } = UseSelectRecipient(navigation);
+  } = UseSelectRecipient();
 
   return (
     <View>
@@ -26,7 +23,7 @@ export default function SelectRecipient({ navigation }) {
         title="수신인 선택"
         buttonLabel="다음"
         onPressBack={() => navigation.goBack()}
-        onPressButton={handlePressNext}
+        onPressButton={() => navigation.navigate('SelectMusic')}
         buttonDisabled={!isNextEnabled}
       />
 
@@ -37,28 +34,21 @@ export default function SelectRecipient({ navigation }) {
         placeholder="편지를 받을 사람을 검색해 주세요."
       />
 
-      {keyword && userList.length === 0 ? (
-        <Empty
-          title='일치하는 사용자가 없습니다.'
-          body='닉네임이나 아이디를 다시 확인해 보세요.'
-        />
-      ) : (
-        <FlatList
-          data={userList}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <ListRow
-              title={item.nickname}
-              subtitle={item.username}
-              img={item.image}
-              imageSource={item.imageSource}
-              caption
-              selected={selectedRecipientId === item.id}
-              onPress={() => handleSelectRecipient(item.id)}
-            />
-          )}
-        />
-      )}
+      <FlatList
+        data={filteredRecipients}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <ListRow
+            title={item.username}
+            subtitle={item.nickname}
+            img={item.profile}
+            imageSource={item.imageSource}
+            caption
+            selected={selectedRecipientId === item.id}
+            onPress={() => handleSelectRecipient(item.id)}
+          />
+        )}
+      />
     </View>
   );
 }
