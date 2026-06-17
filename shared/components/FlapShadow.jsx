a4da63fa533519b37514393d32d1735c5c73fc16
Hodu <9aaa18@gmail.com>
2026-06-17 19:08:31 +0900

    fix: flap 섀도우 수정

--- shared/components/FlapShadow.jsx
+++ shared/components/FlapShadow.jsx
@@ -1,20 +1,20 @@
 import Svg, { Path, Defs, Filter, FeGaussianBlur } from 'react-native-svg';
 
-// viewBox="0 0 325.17 173.2", rotate(180 162.585 86.6) 적용 후의 flap 외곽선
-// 모든 flap 패턴이 동일한 경로를 사용함
+// shadow.middleDown: shadowOffset y=4, shadowRadius=15, shadowOpacity=0.2, shadowColor=#000
+// stdDeviation ≈ shadowRadius / 2 = 7.5
 const FLAP_PATH = 'M311.63,148.09l5.45,17.35H8.09l2.59-10.4c2.86-11.49,9.11-21.85,17.93-29.74L158.96,8.78c1.5-1.34,3.76-1.36,5.29-.04l129.74,112.46c8.26,7.16,14.36,16.46,17.64,26.89Z';
 
 export default function FlapShadow({ width, height }) {
   return (
     <Svg
       width={width}
-      height={height + 20}
-      viewBox="0 -20 325.17 213.2"
+      height={height + 30}
+      viewBox="0 -30 325.17 233.2"
       style={{ position: 'absolute', top: 0, left: 0 }}
     >
       <Defs>
-        <Filter id="flapShadow" x="-10%" y="-20%" width="120%" height="150%">
-          <FeGaussianBlur stdDeviation="6" />
+        <Filter id="flapShadow" x="-10%" y="-20%" width="120%" height="160%">
+          <FeGaussianBlur stdDeviation="7.5" />
         </Filter>
       </Defs>
       <Path
