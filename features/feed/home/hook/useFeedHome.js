61fa89e8711eaf8d1834bd7d72bebdc3789d9343
Hodu <9aaa18@gmail.com>
2026-06-25 11:58:10 +0900

    refactor: feed/home
    더미데이터 삭제, 코드 간략화

--- features/feed/home/hook/useFeedHome.js
+++ features/feed/home/hook/useFeedHome.js
@@ -1,16 +1,18 @@
 import { useState, useCallback, useRef } from 'react';
-import { DUMMY_POSTS } from '../data/feedDummy';
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
+
+// TODO(API): GET /feed?userId=:userId
 
 export default function useFeedHome() {
   const [activeTab, setActiveTab] = useState('추천');
   const [currentIndex, setCurrentIndex] = useState(0);
   const [toastVisible, setToastVisible] = useState(false);
   const toastTimerRef = useRef(null);
-  // 하드코딩
+
+  // TODO(AUTH): 로그인한 사용자 ID로 교체
   const userId = 1;
 
   const {
@@ -20,11 +22,8 @@ export default function useFeedHome() {
     queryKey: ['feeds', userId],
     queryFn: async () => {
       const response = await axios.get(`${API_BASE_URL}/feed`, {
-        params: {
-          userId,
-        },
+        params: { userId },
       });
-
       return response.data;
     },
   });
@@ -40,6 +39,7 @@ export default function useFeedHome() {
   }, []);
 
   return {
+    userId,
     activeTab,
     posts,
     currentIndex,
